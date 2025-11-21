ba3db7d5d3da09ada7932d9216761a85c888ebe9
Sebaas07 <juan.sebas140307@gmail.com>
2025-11-20 19:11:03 -0500

    Pa el arreglo del css

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,19 @@
-const LoginPage = () => {
-  return (
-    <>
-    </>
-  );
-};
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import LoginForm from "../components/LoginForm";
+import "../pages/Login.css";
 
-export default LoginPage;
+export default function Login() {
+  return (
+    <div className="login">
+    <Container fluid>
+      <Row>
+        <Col>
+          <LoginForm/>
+        </Col>
+      </Row>
+    </Container>
+    </div>
+  );
+}
