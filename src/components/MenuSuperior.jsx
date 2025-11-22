453f98573634d6b9c9f1f39c7ff094066ea44138
Sebastian <sotomayo250525@gmail.com>
2025-11-22 14:47:07 -0500

    Ajuste nombre componente

--- src/components/MenuSuperior.jsx
+++ src/components/MenuSuperior.jsx
@@ -1,20 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import './Menu.css';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './MenuSuperior.css';
 
-export default function Menu(){
+export default function MenuSuperior(){
     return( 
-        <Navbar expand="lg" className="menu-navbar" >
-      <Container fluid className="menu-container">
+        <Navbar expand="lg" className="bg-body-tertiary" fill="true">
+      <Container className="menu-container">
         <Navbar.Brand href="#home" className="menu-inicio-logo">Photo Bogotá</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="menu-nav">
+          <Nav className="me-auto">
             <Nav.Link href="#home">¿Quieres ser socio?</Nav.Link>
             <Nav.Link href="#link">Quienes somos</Nav.Link>
-            <button className="menu-button-login rounded-pill" type="submit"><b>Enviar solicitud</b></button>
+            <Nav.Link href="#login" className='inicio-sesion'>Iniciar Sesión</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/pages/PaginaInicioPage.jsx
+++ src/pages/PaginaInicioPage.jsx
@@ -1,21 +1,15 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Menu from '../components/Menu';
-import logo from "../assets/images/logo.png";
+import MenuSuperior from '../components/MenuSuperior';
 import './PaginaInicio.css'
 
 function PaginaInicioPage() {
   return (
     <div className='pagina-inicio-page'> 
       <Container > 
-          <Menu> </Menu>
-          <Row>
-            <h1>Photo Bogotá</h1>
-            <p>Un espacio para compartir miradas fotográficas y redescubrir Bogotá desde diferentes perspectivas</p>
-            <img src={logo} alt="Logo" className="solicitud-form-logo" />
-          </Row>
-          
+          <MenuSuperior> </MenuSuperior>
+          <Col>1 of 2</Col>
           <Col>2 of 2</Col>
         
       </Container>
