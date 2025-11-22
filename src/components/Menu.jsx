dd9787682e64a4e4a6590bb6b6b3eb2116dfb631
Danfel369 <danielfelipecreyes@gmail.com>
2025-11-21 19:55:18 -0500

    Actualización de pagina de inicio y estilos menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,20 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
 import './Menu.css';
 
 export default function Menu(){
     return( 
-        <Navbar expand="lg" className="bg-body-tertiary" fill="true">
-      <Container className="menu-container">
+        <Navbar expand="lg" className="menu-navbar" >
+      <Container fluid className="menu-container">
         <Navbar.Brand href="#home" className="menu-inicio-logo">Photo Bogotá</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="menu-nav">
             <Nav.Link href="#home">¿Quieres ser socio?</Nav.Link>
             <Nav.Link href="#link">Quienes somos</Nav.Link>
-            <Nav.Link href="#login" className='inicio-sesion'>Iniciar Sesión</Nav.Link>
+            <button className="menu-button-login rounded-pill" type="submit"><b>Enviar solicitud</b></button>
           </Nav>
         </Navbar.Collapse>
       </Container>
