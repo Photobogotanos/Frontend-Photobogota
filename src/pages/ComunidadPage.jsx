a87751f0acd9c69c51c0dca914156cad14e33f3c
brandon <sotomayo250525@gmail.com>
2025-11-26 19:00:53 -0500

    Ajuste error Comunidad Page

---
+++ src/pages/ComunidadPage.jsx
@@ -0,0 +1,8 @@
+export default function ComunidadPage() {
+    return (
+        <div>
+            <h1>Comunidad Page</h1>
+            <p>Bienvenido a la página de la comunidad.</p>
+        </div>
+    );
+}
