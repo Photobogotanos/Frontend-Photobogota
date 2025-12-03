b174c6a473bc5a95db41919f7f0fee029530cefe
Sebastián Romero <juan.sebas140307@gmail.com>
2025-12-02 20:58:35 -0500

    Cuadrado lo de las fotos de publicaciones
    WASAAAAAAAAAAA

--- src/components/SpotCard.jsx
+++ src/components/SpotCard.jsx
@@ -1,27 +1,32 @@
-import { Card } from "react-bootstrap";
 import "./SpotCard.css";
 
 export default function SpotCard({ img, title, rating, likes, tags }) {
   return (
-    <Card className="spot-card">
-      <div className="spot-img-container">
-        <img src={img} alt={title} className="spot-img" />
+    <div className="spot-card-horizontal">
 
-        <div className="spot-tags">
+      {/* IMAGEN */}
+      <img src={img} alt={title} className="spot-img-h" />
+
+      {/* CONTENIDO */}
+      <div className="spot-content">
+
+        {/* TAGS */}
+        <div className="spot-tags-h">
           {tags.map((t, i) => (
-            <span key={i} className="spot-tag">{t}</span>
+            <span key={i} className="spot-tag-h">{t}</span>
           ))}
         </div>
-      </div>
 
-      <Card.Body>
-        <Card.Title className="spot-title">{title}</Card.Title>
+        {/* TÍTULO */}
+        <h5 className="spot-title-h">{title}</h5>
 
-        <div className="spot-info">
-          <span className="spot-rating">⭐ {rating}</span>
-          <span className="spot-likes">❤️ {likes}</span>
+        {/* INFO: RATING + LIKES */}
+        <div className="spot-info-h">
+          <span className="spot-rating-h">⭐ {rating}</span>
+          <span className="spot-likes-h">❤️ {likes}</span>
         </div>
-      </Card.Body>
-    </Card>
+
+      </div>
+    </div>
   );
 }
