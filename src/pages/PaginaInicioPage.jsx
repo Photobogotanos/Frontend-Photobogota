ee2c777abc24c3e5e6f20aff7766353b83578375
Sebastian <sotomayo250525@gmail.com>
2025-12-10 12:46:08 -0500

    Ajuste redirigir

--- src/pages/PaginaInicioPage.jsx
+++ src/pages/PaginaInicioPage.jsx
@@ -1,16 +1,26 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import "./PaginaInicioPage.css";
 import PaginaInicioContent from "../components/PaginaInicioContent";
 
 function PaginaInicioPage() {
-  return (
+  const navegar = useNavigate();
 
+  useEffect(() => {
+    const estaLogueado = localStorage.getItem("logueado") === "true";
+    
+    if (estaLogueado) {
+      navegar("/comunidad");
+    }
+  }, [navegar]);
+
+  return (
     <Container fluid className="p-0 mt-5">
       <div className="pagina-inicio-page">
         <PaginaInicioContent></PaginaInicioContent>
       </div>
-    </Container >
-
+    </Container>
   );
 }
 
