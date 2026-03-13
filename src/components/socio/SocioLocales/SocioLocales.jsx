ff1306299eacdaf752155d6a0f6013336f916668
Danfel369 <danielfelipecreyes@gmail.com>
2026-03-13 16:17:35 -0500

    Se agregan las paginas y componentes Socios Locales

---
+++ src/components/socio/SocioLocales/SocioLocales.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap"
+import "./SocioLocales.css";
+
+const SocioLocales = () =>{
+    return(<Container>
+        <header>Saluditos aca va a ir lo de socios </header>
+        <Row>
+            <h2>Administra tus locales</h2>
+        </Row>
+
+    </Container>)
+
+}
+export SocioLocales;
