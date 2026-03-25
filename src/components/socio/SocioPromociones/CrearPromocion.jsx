ff0b4ec4e653185d4fe5c9558f658f1045f60d47
Danfel369 <danielfelipecreyes@gmail.com>
2026-03-24 19:09:35 -0500

    Se crea formularios de promocion
    me estare guiando del formulario de spot

---
+++ src/components/socio/SocioPromociones/CrearPromocion.jsx
@@ -0,0 +1,41 @@
+
+
+
+
+
+// const promoFormReducer = (state, action) =>{
+//     switch (action.type){
+//         case "SET_IMAGENES": return { ...state, imagenes: action.playload };
+//         case "SET_"
+//     }
+
+// }
+
+// function ImageUploader({ previews, onImageChange, onRemove, onNavigate, indice, onSelectIndice})
+//     const [isDragging, setIsDragging] = useState(false);
+//     const inputRef= useRef();
+    
+//     const handleDrop = (e) => {
+//         e.preventDefault();
+//         setIsDragging(false);
+//         const files = Array.from(e.dataTransfer.files).filter((f) => f.type.startsWith("image/"));
+//         if (files.length) onImageChange(files);
+//     };
+
+
+//     const handleFileInput = (e) => {
+//         const files = Array.from(e.target.files);
+//         if (files.length) onImageChange(files);
+//     };
+
+//     const total = previews.length;
+
+// // Componente principal
+
+// export default function CrearPromocion(){
+// const [state, dispatch] = useReducer(spotFormReducer, initialState);
+
+// const handleImagen = (files) => {
+//     const newPreviews = files.map((f))
+// } 
+// }
