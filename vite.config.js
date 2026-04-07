19c00374aa38069f95d91aa10f07ae9eede642ce
Sebastian Sotomayor <139504148+SebasYe05@users.noreply.github.com>
2026-04-06 19:35:00 -0500

    Ajuste: se tiene que usar el nombre del repositorio

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PhotoBogota/',
+  base: '/Frontend-Photobogota/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // Configuración del aliasss
